b3dd120819355154bc6ef949a19e5536807e7cbb
Jorge Minjares <jorge.minjaresps3@gmail.com>
2021-12-13 14:12:44 -0700

    Added Comments

--- IFElseStatments/main.c
+++ IFElseStatments/main.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
-    char name[] = {"Jorge Minjares"};
-    int grade;
+    char name[] = {"Jorge Minjares"}; //Create string
+    int grade;//Creat Integer
     printf("Enter student grade: ");
-    scanf("%i", &grade);
-
+    scanf("%i", &grade);//get integer value
+    //If grade is greater than 70 
     if(grade >= 70){
         printf("%s has passed with a %i\n", name, grade);
     }
+    //Else-if grade is less than 70
     else{
         printf("%s has failed with a %i\n", name, grade);
     }
